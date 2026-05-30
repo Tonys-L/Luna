f67ea14df0ee8827c1d0657d7f772fa564a2bb38
Tony.L <28626915@qq.com>
2026-05-31 01:57:35 +0800

    feat: LunaSpyBridge + ByteKitExitInjector + ByteKitAroundInjector

---
+++ luna-core/src/main/java/fun/efto/luna/core/bytekit/adapter/ByteKitAroundInjector.java
@@ -0,0 +1,22 @@
+/**
+ * @author : Tony.L(<286269159@qq.com>)
+ * @since  : 2026/05/31 22:30
+ */
+package fun.efto.luna.core.bytekit.adapter;
+
+import com.alibaba.bytekit.asm.MethodProcessor;
+import com.alibaba.bytekit.asm.interceptor.InterceptorProcessor;
+import com.alibaba.bytekit.asm.interceptor.parser.DefaultInterceptorClassParser;
+import fun.efto.luna.core.asm.AsmInjectionContext;
+import fun.efto.luna.core.bytekit.interceptor.AroundInterceptor;
+
+import java.util.List;
+
+public class ByteKitAroundInjector extends ByteKitInjectorBase {
+
+    @Override
+    protected List<InterceptorProcessor> createInterceptorProcessors(MethodProcessor methodProcessor, AsmInjectionContext context) {
+        DefaultInterceptorClassParser parser = new DefaultInterceptorClassParser();
+        return parser.parse(AroundInterceptor.class);
+    }
+}
